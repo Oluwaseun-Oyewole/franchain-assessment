8537ed7ff18740ef2a4a7d132dca616e405b5dce
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-05-17 09:47:20 +0100

    complete ui for dashboard screen

--- src/views/dashboard/index.tsx
+++ src/views/dashboard/index.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import Typography from "@/components/custom/typography";
+import { Button } from "@/components/ui/button";
+import { Routes } from "@/routes/routes";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Typography
+        type="h1"
+        children="Welcome to Franchain"
+        weight="medium"
+        variant="displayXs"
+      />
+
+      <Button className="mt-3" onClick={() => navigate(Routes.login)}>
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default Dashboard;
