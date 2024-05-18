10d19c72a633113fe497661fc1215cbd12806400
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-05-18 06:52:36 +0100

    fixed layout for auth screens

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -5,11 +5,11 @@ import { Outlet } from "react-router-dom";
 const AuthLayout = () => {
   return (
     <main className="h-screen flex bg-gradient-to-r lg:from-primary lg:to-light from-light to-light">
-      <div className="w-full max-w-[90%] xl:max-w-[1300px] mx-auto">
-        <header className="h-[15vh]">
+      <div className="w-full">
+        <header className="h-[15vh] w-full max-w-[90%] xl:max-w-[1300px] mx-auto">
           <Navigation />
         </header>
-        <div className="w-full flex items-center justify-center h-[85vh]">
+        <div className="w-full flex items-center justify-center h-[85vh] max-w-[90%] md:max-w-[70%] xl:max-w-[1300px] mx-auto">
           <div className="w-full bg-primary h-full hidden lg:block">
             <GetStarted />
           </div>
